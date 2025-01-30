908b5209bae3caafdd9f7494259011a881ffedaa
LlamasAnonymous <gjbrooks2000@gmail.com>
2025-01-30 06:19:48 -0600

    Versions
    -Now the Version file will retain the newest version from GitHub

--- 3D_File_Mgmt.ps1
+++ 3D_File_Mgmt.ps1
@@ -10,10 +10,10 @@ git clone https://github.com/LlamasAnonymous/Versions.git C:\Versions
 $Local_Version = Get-Content ".\Update\Version.ini"
 $Remote_Version = Get-Content "C:\Versions\3D_File_Mgmt.ini"
 
-$Local_Version = $Local_Version.Replace('.','')
-$Remote_Version = $Remote_Version.Replace('.','')
+$Local_Version1 = $Local_Version.Replace('.','')
+$Remote_Version1 = $Remote_Version.Replace('.','')
 
-$VersionTotal = $Remote_Version - $Local_Version
+$VersionTotal = $Remote_Version1 - $Local_Version1
 
 if ($VersionTotal -ne 0) {
 
