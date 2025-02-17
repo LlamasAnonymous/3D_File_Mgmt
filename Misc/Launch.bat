83e01fb631f028a30b0227853d5046bd6c895bc5
Sqwrl_Whisperer <gjbrooks2000@gmail.com>
2025-02-16 20:40:32 -0600

    Test

--- Misc/Launch.bat
+++ Misc/Launch.bat
@@ -1,4 +1,4 @@
 taskkill /im powershell.exe /f
 taskkill /im pwsh.exe /f
 
-Start /min pwsh.exe -WindowStyle Hidden -file "C:\3D_File_Mgmt\3D_File_Mgmt.ps1"
+Start /min powershell.exe -WindowStyle Hidden -file "C:\3D_File_Mgmt\3D_File_Mgmt.ps1"
