3660deda241d46de5e4a4636e1dcb7a853e94267
Sqwrl_Whisperer <gjbrooks2000@gmail.com>
2025-02-23 16:38:22 -0600

    Update 3D_File_Mgmt.ps1

--- 3D_File_Mgmt.ps1
+++ 3D_File_Mgmt.ps1
@@ -5,8 +5,8 @@ $ErrorActionPreference = 'Continue'
 # $ErrorActionPreference = 'Stop'
 cd $PSScriptRoot
 
-mkdir "$home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Llama_Toolz\Toolz"
-Copy-Item "C:\Llama_Toolz\3D_File_Mgmt\Misc\3D File Mgmt.lnk" "$home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Llama_Toolz\Toolz"
+mkdir "$home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Llama_Toolz"
+Copy-Item "C:\Llama_Toolz\3D_File_Mgmt\Misc\3D File Mgmt.lnk" "$home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Llama_Toolz" -Force
 
 $MaterialsL = "$home\3D_Mgmt\Configs\Materials.ini"
 $ModelingSoftwareL = "$home\3D_Mgmt\Configs\ModelingSoftwareLocation.ini"
